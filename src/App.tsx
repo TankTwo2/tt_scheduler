9a10b57b261b3b4c3d49899b8cd10b19617ad493
Tanktwo <rhkdtjr900@gmail.com>
2020-06-06 12:51:12 +0900

    [tts002] develop ~ structure redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
+import {Paper, Button, TextField} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import CounterContainer from './containers/CounterContainer';
+import useDebounce from "./hooks/useDebounce";
 
+const useStyles = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px',
+  },
+});
+// chrome.storage.sync.set({ "yourBody": "myBody" }, function(){
 function App() {
+  const classes = useStyles();
+  const [inputValue, setInputValue] = useState('');
+
+  const debounceValue = useDebounce(inputValue, 5000);
+
+
+  const onInputChange = useCallback((e)=>{
+    setInputValue(e.target.value)
+  },[]);
+
   return (
-    <div className="App">
-      Hellow World!
-    </div>
+    <>
+      <Paper className={classes.root}>
+        <Button>
+          Hellow World!
+        </Button>
+        <TextField id="standard-basic" label="Standard" value={inputValue} onChange={onInputChange}/>
+        {debounceValue}
+      </Paper>
+      <CounterContainer />
+    </>
   );
 }
 
