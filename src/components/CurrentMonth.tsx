0f7fab96cfd6bbf97c496f42fab2ea8dde93a0d0
Tanktwo <rhkdtjr900@gmail.com>
2020-06-13 16:45:27 +0900

    [tts005] month button

--- src/components/CurrentMonth.tsx
+++ src/components/CurrentMonth.tsx
@@ -1,14 +1,9 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {Grid, Paper, Typography} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import moment from 'moment';
-
-type CurrentMonthPropsType = {
-  onInputChange: (e: any) => void,
-  inputValue: string,
-}
 
 const useStyles = makeStyles({
   root: {
@@ -32,13 +27,46 @@ const useStyles = makeStyles({
 
 export default function CurrentMonth() {
   const classes = useStyles();
-  const [currentYYMM, setCurrentYYMM] = useState<string>(moment().format('YYMM'));
+  const [currentYY, setCurrentYY] = useState(moment().format('YY'));
+  const [currentMM, setCurrentMM] = useState(moment().format('MM'));
+  const [firstWeek, setFirstWeek] = useState(moment(currentMM, 'MM').startOf('month').format('WW'));
+  const [lastWeek, setLastWeek] = useState(moment(currentMM, 'MM').endOf('month').format('WW'));
 
+
+  //주차불러오기
+  useEffect(()=>{
+    let n;
+    for(n=0; n < Number(lastWeek) - Number(firstWeek) + 1; n++){
+      console.log(Number(firstWeek) + n)
+    }
+  },[lastWeek, firstWeek]);
 
   const prevButton = useCallback(()=>{
-    let tempValue = Number(currentYYMM) - 1;
-    setCurrentYYMM(String(tempValue));
-  },[currentYYMM]);
+    let tempMonthValue = String(Number(currentMM) - 1);
+    if(tempMonthValue.length === 1){
+      tempMonthValue = '0' + tempMonthValue
+    }
+    if (tempMonthValue === '00'){
+      setCurrentMM('12');
+      setCurrentYY(String(Number(currentYY) - 1));
+    } else {
+      setCurrentMM(tempMonthValue);
+    }
+  },[currentMM, currentYY]);
+
+  const nextButton = useCallback(()=>{
+    let tempMonthValue = String(Number(currentMM) + 1);
+    if(tempMonthValue.length === 1){
+      tempMonthValue = '0' + tempMonthValue
+    }
+    if (tempMonthValue === '13'){
+      setCurrentMM('01');
+      setCurrentYY(String(Number(currentYY) + 1));
+    } else {
+      setCurrentMM(tempMonthValue);
+    }
+  },[currentMM, currentYY]);
+
 
   return (
     <>
@@ -51,12 +79,12 @@ export default function CurrentMonth() {
           </Grid>
           <Grid key={2} item>
             <Paper className={classes.paper}>
-              <Typography variant="h2" className={classes.monthFont}>{currentYYMM}</Typography>
+              <Typography variant="h2" className={classes.monthFont}>{currentYY + '. ' + currentMM}</Typography>
             </Paper>
           </Grid>
           <Grid key={3} item>
             <Paper className={classes.paper}>
-              <ArrowForwardIosIcon onClick={()=>console.log('next')} className={classes.arrowButton}/>
+              <ArrowForwardIosIcon onClick={nextButton} className={classes.arrowButton}/>
             </Paper>
           </Grid>
         </Grid>
